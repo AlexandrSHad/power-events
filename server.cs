bc3e99c45228888a779cdbed5dbcfbe09924f9cf
Oleksandr Shadurskyi <shadurskij@gmail.com>
2025-12-14 14:56:21 +0100

    added server for events listening

---
+++ server.cs
@@ -0,0 +1,38 @@
+#:sdk Microsoft.NET.Sdk.Web
+#:property TreatWarningsAsErrors=true
+
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLogging(builder => builder.AddConsole());
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolver = ProgramJsonContext.Default;
+});
+
+var app = builder.Build();
+
+app.MapGet("/version", () => "v0.1.0");
+
+app.MapPost("/power-events", (ILogger<Program> logger, PowerEventData eventData) =>
+{
+    logger.LogInformation("[{ReceivedAt}] - Power event received: State={State}, TimeGenerated={TimeGenerated}",
+        DateTime.Now, eventData.State, eventData.TimeGenerated);
+
+    return Results.Accepted();
+});
+
+await app.RunAsync();
+
+class PowerEventData
+{
+    public required string State { get; set; }
+    public required DateTime TimeGenerated { get; set; }
+}
+
+[JsonSerializable(typeof(PowerEventData))]
+partial class ProgramJsonContext : JsonSerializerContext
+{
+}
