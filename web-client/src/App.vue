a0d082d9358babf9b9993172baade647306086e6
Oleksandr Shadurskyi <shadurskij@gmail.com>
2026-01-01 23:55:16 +0100

    feat: show status

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app>
     <AppHeader />
-
     <v-main>
       <v-container class="py-8">
         <v-row justify="center">
@@ -21,14 +20,29 @@
         </v-row>
       </v-container>
     </v-main>
+
+    <!-- Connection status indicator -->
+    <v-snackbar
+      v-model="showStatus"
+      :color="isConnected ? 'success' : 'error'"
+      location="bottom"
+      :timeout="-1"
+    >
+      <div class="d-flex align-center">
+        <v-icon :icon="isConnected ? 'mdi-wifi' : 'mdi-wifi-off'" class="mr-2"></v-icon>
+        <span>{{ isConnected ? '🚀 Houston, we have connection!' : '👻 Server ghosted us...' }}</span>
+      </div>
+    </v-snackbar>
   </v-app>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useEventSource } from './composables/useEventSource'
 import AppHeader from './components/AppHeader.vue'
 import StatusCard from './components/StatusCard.vue'
 import HistoryTimeline from './components/HistoryTimeline.vue'
 
-const { currentStatus, eventHistory } = useEventSource()
+const { currentStatus, eventHistory, isConnected } = useEventSource()
+const showStatus = ref(true)
 </script>
