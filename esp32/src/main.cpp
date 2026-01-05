95546c234f88d7da2d14aaad82070d2486d48e82
Oleksandr Shadurskyi <shadurskij@gmail.com>
2026-01-05 17:31:04 +0100

    refactor: encapsulate WiFi connection logic into separate class

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,59 +1,20 @@
 #include <Arduino.h>
-#include <WiFiManager.h>
-#include <esp_wifi.h>
+#include "WiFiConnection.h"
 
-// Built-in RGB LED pin
 #define RGB_LED_PIN 48
 
-WiFiManager wm;
-
-bool hasWiFiCredentials() {
-  wifi_config_t conf;
-  esp_wifi_get_config(WIFI_IF_STA, &conf);
-  return strlen((char*)conf.sta.ssid) > 0;
-}
+WiFiConnection wifi("ESP32-S3-01-setup", RGB_LED_PIN);
 
 void setup() {
   pinMode(RGB_LED_PIN, OUTPUT);
-
-  //wm.resetSettings();
-
-  // Must init WiFi in STA mode before checking credentials
-  WiFi.mode(WIFI_STA);
-
-  if (hasWiFiCredentials()) {
-    // Has saved credentials - show yellow
-    neopixelWrite(RGB_LED_PIN, 5, 5, 0);
-    delay(2000);
-  } else {
-    // No credentials - show blue (config portal needed)
-    neopixelWrite(RGB_LED_PIN, 0, 0, 5);
-  }
-
-  // Starts config portal if no saved credentials, otherwise connects automatically
-  // "ESP32-Setup" is the AP name users will see
-  bool connected = wm.autoConnect("ESP32-S3-01-setup");
-
-  if (!connected) {
-    // Failed to connect - restart
-    ESP.restart();
-  }
+  wifi.connect();
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    // Connected - blink green
-    neopixelWrite(RGB_LED_PIN, 0, 5, 0);
-    delay(500);
+  // Normal operation - blink green
+  neopixelWrite(RGB_LED_PIN, 0, 5, 0);
+  delay(500);
 
-    neopixelWrite(RGB_LED_PIN, 0, 0, 0);
-    delay(2000);
-  } else {
-    // Not connected - blink red
-    neopixelWrite(RGB_LED_PIN, 5, 0, 0);
-    delay(200);
-
-    neopixelWrite(RGB_LED_PIN, 0, 0, 0);
-    delay(200);
-  }
+  neopixelWrite(RGB_LED_PIN, 0, 0, 0);
+  delay(2000);
 }
